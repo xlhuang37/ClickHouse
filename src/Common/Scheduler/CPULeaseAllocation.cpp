86fea5104d636f1a54c108e69d19640ec468f6cd
Xiaolong Huang <xlhuang2002@gmail.com>
2026-05-07 21:15:08 -0400

    regress to see what caused performance diff

--- src/Common/Scheduler/CPULeaseAllocation.cpp
+++ src/Common/Scheduler/CPULeaseAllocation.cpp
@@ -641,6 +641,7 @@ bool CPULeaseAllocation::schedule(std::unique_lock<std::mutex> &)
     else
         priority.value = consumed_ns;
 
+    priority.value = 10;
     ResourceCost cost = settings.quantum_ns + std::max<ResourceCost>(0, consumed_ns - requested_ns);
     requested_ns += cost;
     if (requests.enqueue(cost, requested_ns, priority))
