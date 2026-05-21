1114fd6b9203f367fce0f09f5ccd88810ffc7336
Xiaolong Huang <xlhuang2002@gmail.com>
2026-05-21 17:49:23 -0400

    grammar

--- src/Common/Scheduler/CPULeaseAllocation.cpp
+++ src/Common/Scheduler/CPULeaseAllocation.cpp
@@ -620,7 +620,7 @@ bool CPULeaseAllocation::schedule(std::unique_lock<std::mutex> &)
     if (settings.get_tasks_count)
     {
         size_t tasks_count = settings.get_tasks_count();
-        cap = std::max(std::min(max_threads, tasks_count), 4);
+        cap = std::max<size_t>(std::min<size_t>(max_threads, tasks_count), 4);
     }
     if (allocated == max_threads || threads.running_count >= cap || shutdown)
         return true;
