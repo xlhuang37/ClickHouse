2fa28c0d13a08c5b4f195f8266e09d514a4f069a
Xiaolong Huang <xlhuang2002@gmail.com>
2026-05-26 08:26:46 -0400

    Redefining Cap

--- src/Common/Scheduler/CPULeaseAllocation.cpp
+++ src/Common/Scheduler/CPULeaseAllocation.cpp
@@ -628,7 +628,7 @@ bool CPULeaseAllocation::schedule(std::unique_lock<std::mutex> &)
     size_t cap = max_threads;
     if (settings.get_tasks_count)
     {
-        size_t tasks_count = settings.get_tasks_count();
+        size_t tasks_count = settings.get_tasks_count() + threads.running_count;
         cap = std::max<size_t>(std::min<size_t>(max_threads, tasks_count), 2);
     }
     if (allocated == max_threads || shutdown)
@@ -656,7 +656,7 @@ bool CPULeaseAllocation::schedule(std::unique_lock<std::mutex> &)
 
     ResourceCost cost = settings.quantum_ns + std::max<ResourceCost>(0, consumed_ns - requested_ns);
     requested_ns += cost;
-    const auto enqueue_result = requests.enqueue(cost, requested_ns, priority, threads.running_count >= cap);
+    const auto enqueue_result = requests.enqueue(cost, requested_ns, priority, cap < threads.running_count);
     if (enqueue_result == RequestChain::EnqueueResult::Enqueued)
     {
         scheduled_increment.add();
